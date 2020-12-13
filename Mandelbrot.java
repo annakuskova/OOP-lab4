099a364e57abd3dce2f42b697566db61ee52b2d9
annakuskova <55314661+annakuskova@users.noreply.github.com>
2020-12-13 08:31:10 +0300

    Update Mandelbrot.java

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -4,13 +4,7 @@ import java.awt.geom.Rectangle2D;
 public class Mandelbrot extends FractalGenerator {
     public static final int MAX_ITERATIONS = 2000;
 
-    /*static Complex mandelbrotFunction(Complex compl, int n) {
-        if (n == 0) {
-            return new Complex(0, 0);
-        } else {
-            return mandelbrotFunction(compl, n - 1).multiple(mandelbrotFunction(compl, n - 1)).plus(compl);
-        }
-    }*/
+  
 
     @Override
     public void getInitialRange(Rectangle2D.Double range) {
@@ -32,16 +26,6 @@ public class Mandelbrot extends FractalGenerator {
 
         if (count == MAX_ITERATIONS) return -1;
         return count;
-        /*for (int i = 0; i < MAX_ITERATIONS; i++) {
-            Complex z = mandelbrotFunction(c, i);
-            if (z.abs() * z.abs() > 4) {
-                count = i;
-                break;
-            }
-        }
-
-        if (count == 0) {
-            return -1;
-        } else return count;*/
+        
     }
 }
