0fe992d68b67fbdef7f8fa399acaf0dbdde68eba
annakuskova <55314661+annakuskova@users.noreply.github.com>
2020-12-13 08:29:30 +0300

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -17,28 +17,4 @@ public class Complex {
         im = imagine;
     }
 
-
-
-    /*
-
-                                  // Complex summa = z.plus(new Complex(1, 2));  (summa == 2 + i*3.43)
-    Complex plus(Complex z) {     // Complex z = new Complex(1, 1.43);
-        return new Complex(this.re + z.re, this.im + z.im);
-    }
-
-    Complex minus(Complex z) {
-        return new Complex(this.re - z.re, this.im - z.im);
-    }
-
-    Complex multiple(Complex z) {
-        return new Complex(this.re * z.re - this.im * z.im, this.re * z.im + z.re * this.im);
-    }
-
-    Complex divideBy(Complex z) {
-        return new Complex((this.re * z.re + this.im * z.im)/(z.re*z.re + z.im*z.im), (z.re*this.im - this.re*z.im)/(z.re*z.re + z.im*z.im));
-    }
-
-    double abs() {          // double module = z.abs();     if (z.abs() > 2)
-        return Math.sqrt(this.re*this.re + this.im*this.im);
-    }*/
 }
